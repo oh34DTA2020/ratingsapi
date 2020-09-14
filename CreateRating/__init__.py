cde12a8dc426afb3ce603b46fb9389fb08ba82d8
krishnaji <krishna@MININT-1T52PI3.fareast.corp.microsoft.com>
2020-09-14 18:23:16 -0400

    Init

--- CreateRating/__init__.py
+++ CreateRating/__init__.py
@@ -6,15 +6,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
+    req_body = req.get_json()
+    
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
