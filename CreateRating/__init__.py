052a37a8eaeb3a4082bce4a169d31c9ed30745a4
Ken Ward <kenrward@gmail.com>
2020-09-15 20:17:12 -0400

    validate user and product

--- CreateRating/__init__.py
+++ CreateRating/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+import uuid
 
 import azure.functions as func
 
@@ -6,12 +8,42 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    req_body = req.get_json()
+    userId = req.params.get('userId')
+    productId = req.params.get('productId')
+    locationName = req.params.get('locationName')
+    rating = req.params.get('rating')
+    userNotes = req.params.get('userNotes')
+
+    # Validate both userId and productId by calling the existing API endpoints. You can find a user id to test with from the sample payload above
+    uidurl = "https://serverlessohuser.trafficmanager.net/api/GetUser"
+    response1 = requests.get(uidurl, params={'userId': userId})
+
+    if response1:
+        logging.info('Success User Found!')
+    else:
+        logging.info('User Not Found.')
     
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    produrl = "https://serverlessohproduct.trafficmanager.net/api/GetProduct"
+    response2 = requests.get(produrl, params={'productId': productId})
+
+    if response2:
+        logging.info('Success Product Found!')
+    else:
+        logging.info('Product Not Found.')
+
+    # Add a property called id with a GUID value
+    id = uuid.uuid4() 
+    # Add a property called timestamp with the current UTC date time
+    # Validate that the rating field is an integer from 0 to 5
+    # Use a data service to store the ratings information to the backend
+    # Return the entire review JSON payload with the newly created id and timestamp
+
+    
+    if userId:
+        return func.HttpResponse(f"Hello, {userId}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"response : {x}",
              status_code=200
         )
+
