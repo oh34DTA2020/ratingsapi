409ca684342747115905d6753c59fd0fe908dd45
Shrikrishna Khose <shkhose@microsoft.com>
2020-09-16 11:38:57 -0400

    GetRating Updated

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -1,18 +1,6 @@
 import logging
-import requests
-
 import azure.functions as func
 
-
 def main(req: func.HttpRequest,getratings: func.DocumentList) -> func.HttpResponse:
-    #req_body = req.get_json()
-    logging.info("Found Ratings items,%s", getratings)
-    #if not req_body:
-    #    logging.warning("Ratings item not found")
-    #else:
-    #    logging.info("Found Ratings items,%s",
-    #                 req_body)
-
-
-    #return func.HttpResponse(req_body)
-    return("OK")
+    logging.info("Found Ratings items,%s", getratings[0].to_json())
+    return func.HttpResponse(getratings[0].to_json())
